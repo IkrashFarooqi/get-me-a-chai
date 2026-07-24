bc196ba083639e8bf10821e9822b7f6504e30f64
Ikrash Farooqi <ikrashfarooqibutt@gmail.com>
2026-07-24 16:25:39 +0500

    first commit

--- app/page.js
+++ app/page.js
@@ -2,64 +2,41 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.js file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
+    <>
+      <div className="mb-10">
+        <div className="text-white flex flex-col justify-center gap-4 items-center h-[44vh]">
+          <div className="font-bold text-5xl flex justify-center items-center gap-2">Get me a chai <span><img className="w-16 pb-4" src="tea.gif" alt="Tea-gif" /></span></div>
+          <p>
+            A crowd funding platform for creators. Get funded by your fans and followers. Start now!
           </p>
+          <div className="gap-3 flex">
+            <button type="button" className="text-white bg-linear-to-br from-purple-600 to-blue-500 hover:bg-linear-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-4 py-2.5 text-center leading-5 cursor-pointer me-2 mb-2">Start here</button>
+            <button type="button" className="text-white bg-linear-to-br from-purple-600 to-blue-500 hover:bg-linear-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-4 py-2.5 text-center leading-5 cursor-pointer me-2 mb-2">Read more</button>
+          </div>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+        <div className="bg-white h-1 opacity-10"></div>
+
+        <div className="text-white container mx-auto">
+          <h1 className="text-2xl font-bold text-center my-12 " >Your fans can buy you a chai</h1>
+          <div className="flex gap-5 justify-around">
+            <div className="item space-y-3 flex flex-col justify-center items-center">
+              <img width={88} className="bg-slate-400 rounded-full p-2" src="man.gif" alt="" />
+              <p className="font-bold">Fans want to help</p>
+              <p>Your fans are avaliable for you to help you</p>
+            </div>
+            <div className="item space-y-3 flex flex-col justify-center items-center">
+              <img width={88} className="bg-slate-400 rounded-full p-2" src="coin.gif" alt="" />
+              <p className="font-bold">Fans want to help</p>
+              <p>Your fans are avaliable for you to help you</p>
+            </div>
+            <div className="item space-y-3 flex flex-col justify-center items-center">
+              <img width={88} className="bg-slate-400 rounded-full p-2" src="group.gif" alt="" />
+              <p className="font-bold">Fans want to help</p>
+              <p>Your fans are avaliable for you to help you</p>
+            </div>
+          </div>
         </div>
-      </main>
-    </div>
+      </div>
+    </>
   );
 }
---
+++ components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <footer className='bg-blue-950 text-white flex justify-center items-center px-4 h-8'>
+            <p>Copyright &copy; Get me a Chai - All rights reserved</p>
+        </footer>
+    )
+}
+
+export default Footer
