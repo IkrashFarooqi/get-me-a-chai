3fae2c57433c9510fe0fdaddf69403a715028a12
Ikrash Farooqi <ikrashfarooqibutt@gmail.com>
2026-07-27 21:14:35 +0500

    fix:  fixied navbar issue

--- components/Navbar.js
+++ components/Navbar.js
@@ -20,7 +20,7 @@ const Navbar = () => {
                 <li>Login</li>
             </ul> */}
             <div className="relative">
-                {session && <><button onClick={() => { setShowdropdown(!showdropdown) }} onBlur={() => { setTimeout(() => { setShowdropdown(false) }, 100) }} id="dropdownDefaultButton" data-dropdown-toggle="dropdown" className="inline-flex items-center justify-center text-white bg-blue-600 box-border border border-transparent hover:bg-blue-700 focus:ring-4 focus:ring-brand-medium shadow-xs font-medium leading-5 cursor-pointer rounded-lg text-sm px-4 py-2.5 focus:outline-none mx-2" type="button">
+                {session && <><button onClick={() => { setShowdropdown(!showdropdown) }} id="dropdownDefaultButton" data-dropdown-toggle="dropdown" className="inline-flex items-center justify-center text-white bg-blue-600 box-border border border-transparent hover:bg-blue-700 focus:ring-4 focus:ring-brand-medium shadow-xs font-medium leading-5 cursor-pointer rounded-lg text-sm px-4 py-2.5 focus:outline-none mx-2" type="button">
                     Welcom {session.user.email}
                     <svg className="w-4 h-4 ms-1.5 -me-0.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 9-7 7-7-7" /></svg>
                 </button>
